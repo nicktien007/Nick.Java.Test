8d577addb742de6c98bb45c6efa1d71614bb08a5
nick <trojanhorse0077@gmail.com>
2021-12-24 23:48:53 +0800

    Initial commit

---
+++ src/test/java/com/nick/javatest/ElasticsearchTest.java
@@ -0,0 +1,13 @@
+package com.nick.javatest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ElasticsearchTest {
+
+    @Test
+    void test() {
+        log.info("test");
+    }
+}
