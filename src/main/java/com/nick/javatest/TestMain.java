d94e8b88514719df7f2a7b07afa0786bb508f2ec
nick <trojanhorse0077@gmail.com>
2022-01-25 19:44:14 +0800

    add testOpen

---
+++ src/main/java/com/nick/javatest/TestMain.java
@@ -0,0 +1,72 @@
+package com.nick.javatest;
+
+import edu.stanford.nlp.ie.util.RelationTriple;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.naturalli.NaturalLogicAnnotations;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.simple.Document;
+import edu.stanford.nlp.simple.Sentence;
+import edu.stanford.nlp.util.CoreMap;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Collection;
+import java.util.Properties;
+
+@Slf4j
+public class TestMain {
+    public static void main(String[] args) {
+        testOpenIE_1();
+//        testOpenIE_2();
+    }
+
+    private static void testOpenIE_1() {
+        // Create the Stanford CoreNLP pipeline
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize,ssplit,pos,lemma,depparse,natlog,openie");
+
+        StanfordCoreNLP pipeline = null;
+        try {
+            pipeline = new StanfordCoreNLP(props);
+
+        }catch (Exception e){
+            log.error(e.toString());
+        }
+
+        // Annotate an example document.
+//        Annotation doc = new Annotation("Obama was born in Hawaii. He is our president.");
+        Annotation doc = new Annotation("Obama was born in Hawaii. He is our president.");
+        pipeline.annotate(doc);
+
+        // Loop over sentences in the document
+        for (CoreMap sentence : doc.get(CoreAnnotations.SentencesAnnotation.class)) {
+            // Get the OpenIE triples for the sentence
+            Collection<RelationTriple> triples =
+                    sentence.get(NaturalLogicAnnotations.RelationTriplesAnnotation.class);
+            // Print the triples
+            for (RelationTriple triple : triples) {
+                System.out.println(triple.confidence + "\t" +
+                        triple.subjectLemmaGloss() + "\t" +
+                        triple.relationLemmaGloss() + "\t" +
+                        triple.objectLemmaGloss());
+            }
+        }
+    }
+
+    private static void testOpenIE_2() {
+        // Create a CoreNLP document
+        Document doc = new Document("Obama was born in Hawaii. He is our president.");
+
+        // Iterate over the sentences in the document
+        for (Sentence sent : doc.sentences()) {
+            // Iterate over the triples in the sentence
+            for (RelationTriple triple : sent.openieTriples()) {
+                // Print the triple
+                System.out.println(triple.confidence + "\t" +
+                        triple.subjectLemmaGloss() + "\t" +
+                        triple.relationLemmaGloss() + "\t" +
+                        triple.objectLemmaGloss());
+            }
+        }
+    }
+}
