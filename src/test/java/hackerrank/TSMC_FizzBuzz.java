87020feeba23a6a955fa68124e0afe175bb5c762
nick <trojanhorse0077@gmail.com>
2022-03-07 15:56:10 +0800

    update TSMC_FizzBuzz

--- src/test/java/hackerrank/TSMC_FizzBuzz.java
+++ src/test/java/hackerrank/TSMC_FizzBuzz.java
@@ -9,10 +9,28 @@ public class TSMC_FizzBuzz {
 
     @Test
     void test_1() {
-        mainProcessor(1);
+        mainProcessor(15);
     }
 
     void mainProcessor(Integer n){
-        System.out.println(n);
+        for (int i = 1; i <= n; i++) {
+
+            if (i % 5 == 0 && i % 3 == 0) {
+                System.out.println("FizzBuzz");
+                continue;
+            }
+
+            if (i % 3 == 0) {
+                System.out.println("Fizz");
+                continue;
+            }
+
+            if (i % 5 == 0) {
+                System.out.println("Buzz");
+                continue;
+            }
+
+            System.out.println(i);
+        }
     }
 }
